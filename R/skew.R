c12c55ff239cfb1fcc72e103fb9cd39d46c53eae
LukePapayoanou <l.papayoanou@email.msmary.edu>
2025-06-16 22:21:22 -0700

    Add skew function

---
+++ R/skew.R
@@ -0,0 +1,17 @@
+#' Compute Sample Skewness
+#'
+#' Calculates the skewness of a numeric vector.
+#' A positive value indicates right skew (long right tail), while a negative value
+#' indicates left skew (long left tail). A zero value represents symmetry
+#'
+#' @param x A numeric vector.
+#'
+#' @return A single numeric value representing the skewness of the distribution.
+#'
+#' @export
+#'
+
+skew <- function(x) {
+  z <- (x-mean(x,na.rm=TRUE))/sd(x,na.rm=TRUE)
+  mean(z^3,na.rm=TRUE)
+}
