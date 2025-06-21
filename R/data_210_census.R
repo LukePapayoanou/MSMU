bf71066c9f9e555f8f4aaa88181fb6493d078d91
LukePapayoanou <l.papayoanou@email.msmary.edu>
2025-06-21 15:26:27 -0700

    Add and document data_210_census dataset

---
+++ R/data_210_census.R
@@ -0,0 +1,13 @@
+#' Synthetic Census dataset
+#'
+#'
+#' @format A data frame with 1000 rows and 5 columns:
+#' \describe{
+#'   \item{age}{Persons Age (integer)}
+#'   \item{Gender}{Persons Gender (character)}
+#'   \item{degree}{Persons level of education (character)}
+#'   \item{salary}{Persons Yearly Salary (integer)}
+#'   \item{height}{Persons Height in inches (integer)}
+#' }
+#' @source Dr.McCurdy, cleaned and processed by Dr.McCurdy
+"data_210_census"
