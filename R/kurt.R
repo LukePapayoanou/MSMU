d21aafce45a2b20de289ad79a595513ecec00e87
LukePapayoanou <l.papayoanou@email.msmary.edu>
2025-06-29 14:28:29 -0700

    Kurtosis formula added

--- R/kurt.R
+++ R/kurt.R
@@ -5,6 +5,19 @@
 #' positive values indicate heavier tails (leptokurtic), and negative
 #' values indicate lighter tails (platykurtic).
 #'
+#' @details
+#' The z-scores are computed as:
+#' \deqn{z_i = \frac{x_i - \bar{x}}{sd}}
+#'
+#' The kurtosis is then calculated as:
+#' \deqn{\text{Kurtosis} = \frac{1}{n} \sum_{i=1}^{n} z_i^4 - 3}
+#'
+#' Where:
+#' - \eqn{\bar{x}} is the mean of \eqn{x},
+#' - \eqn{sd} is the standard deviation of \eqn{x},
+#' - and \eqn{n} is the number of observations.
+#'
+#'
 #' @param x A numeric vector.
 #'
 #' @return A single numeric value representing the kurtosis
