028c23d591e238d71ea666ee60f25f9c62bd8b22
LukePapayoanou <l.papayoanou@email.msmary.edu>
2025-06-29 15:01:09 -0700

    Updated position stats documentation

--- R/position_stats.R
+++ R/position_stats.R
@@ -1,7 +1,18 @@
 #' Computes Position Statics, Quintiles and Quartiles
 #'
-#' Calculates the quintiles and quartiles of a numeric vector using the
-#' 'quintile()' function. NA's are removed
+#' Calculates the quintiles, including quartiles(data is split in 4 equal parts)
+#' and quintiles(data is split in 5 equal parts) of a numeric vector using the
+#' 'quantile()' function. NA's are removed.
+#'
+#' @details
+#' **Percentiles** are values that divide a dataset into 100 equal parts, each representing 1% of the distribution.
+#'   For example, the 25th percentile is the value below which 25% of the data fall.
+#'
+#' **Quartiles** are special percentiles that divide the data into four equal groups:
+#'   Q1 (25th percentile), Q2 (50th percentile or median), Q3 (75th percentile).
+#'
+#' **Quintiles** divide data into five equal groups, each representing 20% of the distribution:
+#'   20th percentile, 40th, 60th, 80th percentiles split the data into quintiles.
 #'
 #' @param x A numeric vector.
 #'
