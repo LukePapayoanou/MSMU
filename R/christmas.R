4fdae2630265462f7442f375eae03e86ccf68428
LukePapayoanou <l.papayoanou@email.msmary.edu>
2025-06-30 15:30:18 -0700

    Adding christmas data and documentation

---
+++ R/christmas.R
@@ -0,0 +1,26 @@
+#' Christmas data
+#'
+#' Santa's dataset, exploring if Santa gives children presents based a variety of variables!
+#'
+#'
+#' @format A data frame with 1000 rows and 15 columns:
+#' \describe{
+#'   \item{Gender}{Gender (character)}
+#'   \item{Toy_Count}{Number of toys (integer)}
+#'   \item{Chores_Completed}{Number of Chores completed (numeric)}
+#'   \item{Favorite_Color}{Childs Favorite color (character)}
+#'   \item{Helping_Hand}{Childs helping hand number/score (integer)}
+#'   \item{Complaints_Recieved}{Number of complaints child says (numeric)}
+#'   \item{Tantrum_Count}{Number of Tantrums child has (integer)}
+#'   \item{Rule_Breaks}{Number of rule breaking child does (numeric)}
+#'   \item{Sharing_Behavior}{Childs willingness to share (numeric)}
+#'   \item{Hours_of_Sleep}{Childs average hours of sleep per night (numeric)}
+#'   \item{Screen_Time}{Childs average hours of screen time (numeric)}
+#'   \item{School_Grade}{Childs school grade (numeric)}
+#'   \item{Parent_Presence}{Childs parent presence (numeric)}
+#'   \item{Greed_Score}{Santas numeric system for labeling childrens greed (numeric)}
+#'   \item{Outcome}{Whether a child gets a present or coal (character)}
+#' }
+#'
+#' @source Santa
+"christmas"
