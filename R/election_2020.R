19b6e8b636aebf72dc01b354a4149ce4c4bfd8f8
LukePapayoanou <l.papayoanou@email.msmary.edu>
2025-06-30 16:37:46 -0700

    adding 2020 election data and documentation

---
+++ R/election_2020.R
@@ -0,0 +1,16 @@
+#' 2020 election data
+#'
+#'
+#' @format A data frame with 32177 rows and 7 columns:
+#' \describe{
+#'   \item{state}{State (character)}
+#'   \item{state_ev}{State electoral votes (integer)}
+#'   \item{county}{County name (character)}
+#'   \item{candidate}{Candidate name (character)}
+#'   \item{party}{Candidate party (character)}
+#'   \item{total_votes}{Total number of votes (integer)}
+#'   \item{won}{True or false for the candidate to win the county (logical)}
+#' }
+#'
+#' @source 2020 election data
+"election_2020"
