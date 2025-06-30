0c8f50aa22445ab864fc7fea5300cdbf27462504
LukePapayoanou <l.papayoanou@email.msmary.edu>
2025-06-29 17:55:17 -0700

    Adding housing data and documentation

---
+++ R/housing_data.R
@@ -0,0 +1,15 @@
+#' Housing data
+#'
+#'
+#' @format A data frame with 98 rows and 6 columns:
+#' \describe{
+#'   \item{price}{Home price (numeric)}
+#'   \item{bed}{Number of bedrooms (integer)}
+#'   \item{bath}{Number of bathrooms (numeric)}
+#'   \item{area}{Square footage (integer)}
+#'   \item{year_built}{Date house was built (integer)}
+#'   \item{lot}{lot size (numeric)}
+#' }
+#'
+#' @source housing data
+"housing_data"
