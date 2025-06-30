f729175efbd4dbc08e928e530009cfb887fd15b4
LukePapayoanou <l.papayoanou@email.msmary.edu>
2025-06-29 22:59:31 -0700

    Adding college data and documentation

---
+++ R/college_data.R
@@ -0,0 +1,26 @@
+#' College data
+#'
+#'
+#' @format A data frame with 777 rows and 16 columns:
+#' \describe{
+#'   \item{Name}{College name (character)}
+#'   \item{Region}{US region (character)}
+#'   \item{Accept}{Acceptance (integer)}
+#'   \item{Enroll}{Enrollment (integer)}
+#'   \item{Top10perc}{Percent of students that were top 10 in highschool class (integer)}
+#'   \item{Top25perc}{Percent of students that were top 25 in highschool class (integer)}
+#'   \item{F.Undergrad}{Full time undergrad (integer)}
+#'   \item{P.Undergrad}{Part time undergrad (integer)}
+#'   \item{Outstate}{Number of Out of state students (integer)}
+#'   \item{Room.Board}{Annual room and board price (integer)}
+#'   \item{PhD}{Percentage of Faculty with a PhD (integer)}
+#'   \item{Terminal}{Percentage of Faculty with a terminal degree (integer)}
+#'   \item{S.F.Ratio}{Student Faculty ratio (numeric)}
+#'   \item{test.preparation.course}{Student test prep level (character)}
+#'   \item{perc.alumni}{Percent of alumni who donate to the college (integer)}
+#'   \item{Expend}{Instructional expenditure per student (integer)}
+#'   \item{Grad.Rate}{Graduation Rate (integer)}
+#' }
+#'
+#' @source college data
+"college_data"
