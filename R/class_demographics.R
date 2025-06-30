ab72d632630b9cb2f10f6bb4b0e1838223d76887
LukePapayoanou <l.papayoanou@email.msmary.edu>
2025-06-29 17:35:34 -0700

    Adding class demographics data and documentation

---
+++ R/class_demographics.R
@@ -0,0 +1,15 @@
+#' Class demographics
+#'
+#'
+#' @format A data frame with 50 rows and 6 columns:
+#' \describe{
+#'   \item{name}{Persons name (character)}
+#'   \item{ages}{Persons age (int)}
+#'   \item{state}{Persons state (character)}
+#'   \item{year}{Persons year in college (character)}
+#'   \item{major}{Persons major (character)}
+#'   \item{sport}{Binary Sport, 1(yes) or 0(no) (integer)}
+#' }
+#'
+#' @source class data
+"class_demographics"
