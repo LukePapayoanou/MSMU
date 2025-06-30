7895f21d61dc11bfd3a8b95c5a296d6d3f0d9557
LukePapayoanou <l.papayoanou@email.msmary.edu>
2025-06-29 18:03:30 -0700

    Adding exam scores and documentation

---
+++ R/exam_scores.R
@@ -0,0 +1,17 @@
+#' Exam data
+#'
+#'
+#' @format A data frame with 99 rows and 8 columns:
+#' \describe{
+#'   \item{gender}{Students gender (character)}
+#'   \item{race.ethnicity}{Students race/ethnicity (character)}
+#'   \item{parental.level.of.education}{Parents level of education (character)}
+#'   \item{lunch}{Students lunch plan (character)}
+#'   \item{test.preparation.course}{Student test prep level (character)}
+#'   \item{math.score}{Students math score (integer)}
+#'   \item{reading.score}{Students reading score (integer)}
+#'   \item{writing.score}{Students writing score (integer)}
+#' }
+#'
+#' @source Exam data
+"exam_scores"
