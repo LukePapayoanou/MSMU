8a5e77a1c3cdf4865a3e0cbccc9c808a50b60c3e
LukePapayoanou <l.papayoanou@email.msmary.edu>
2025-06-30 16:29:15 -0700

    adding reaction time data and documentation

---
+++ R/reaction_time.R
@@ -0,0 +1,15 @@
+#' reaction data
+#'
+#'
+#' @format A data frame with 100 rows and 6 columns:
+#' \describe{
+#'   \item{person}{Person id (integer)}
+#'   \item{color}{color (character)}
+#'   \item{left}{left (numeric)}
+#'   \item{right}{right (numeric)}
+#'   \item{age}{Person age (numeric)}
+#'   \item{Gender}{Person gender (character)}
+#' }
+#'
+#' @source reaction data
+"reaction_time"
