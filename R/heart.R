91a688864497d17a66195c84dcd9c69e80d569cf
LukePapayoanou <l.papayoanou@email.msmary.edu>
2025-06-30 16:01:07 -0700

    Adding heart data and documentation

---
+++ R/heart.R
@@ -0,0 +1,22 @@
+#' heart data
+#'
+#'
+#' @format A data frame with 303 rows and 14 columns:
+#' \describe{
+#'   \item{Age}{Patients age (integer)}
+#'   \item{Sex}{Patients Sex (1 = Male, 0 = Female) (integer)}
+#'   \item{ChestPain}{Chest pain type (character)}
+#'   \item{RestBP}{Resting blood pressure (in mm Hg on admission to the hospital) (integer)}
+#'   \item{Chol}{Serum cholestoral in mg/dl (integer)}
+#'   \item{Fbs}{fasting blood sugar &gt; 120 mg/dl (1 = true; 0 = false) (integer)}
+#'   \item{RestECG}{Resting electrocardiographic results (integer)}
+#'   \item{MaxHR}{Maximum heart rate achieved (integer)}
+#'   \item{ExAng}{Exercise induced angina (1 = yes; 0 = no) (integer)}
+#'   \item{Oldpeak}{ST depression induced by exercise relative to rest (numeric)}
+#'   \item{Ca}{Number of major vessels (0-3) colored by flourosopy (integer)}
+#'   \item{Thal}{Thal condition (character)}
+#'   \item{AHD}{Atherosclerosis Heart Disease condition (character)}
+#' }
+#'
+#' @source Heart Disease
+"heart"
