55154116f606a8998357f3054d587d29d9eed3dd
LukePapayoanou <l.papayoanou@email.msmary.edu>
2025-06-30 16:47:12 -0700

    adding soccer data and documentation

---
+++ R/soccer.R
@@ -0,0 +1,15 @@
+#' Historic soccer data
+#'
+#'
+#' @format A data frame with 13750 rows and 6 columns:
+#' \describe{
+#'   \item{X}{numbered list (integer)}
+#'   \item{date}{Date of match (character)}
+#'   \item{home_team}{Home team name (character)}
+#'   \item{away_team}{Away team name (character)}
+#'   \item{home_score}{Home teams goal count (integer)}
+#'   \item{away_score}{Away teams goal count (integer)}
+#' }
+#'
+#' @source historic soccer data
+"soccer"
