13c0254c6543de3da64dd3f1dfdf257934297532
LukePapayoanou <l.papayoanou@email.msmary.edu>
2025-06-30 16:56:45 -0700

    adding mini project exam data and documentation

---
+++ R/exam_data.R
@@ -0,0 +1,17 @@
+#' Mini Project exam data
+#'
+#'
+#' @format A data frame with 1000 rows and 8 columns:
+#' \describe{
+#'   \item{gender}{Students gender (character)}
+#'   \item{race.ethnicity}{Students race/ethnicity (character)}
+#'   \item{parental.level.of.education}{Parents level of education (character)}
+#'   \item{lunch}{Students lunch plan (character)}
+#'   \item{test.preparation.course}{Student test prep level (character)}
+#'   \item{math.score}{Students math score (integer)}
+#'   \item{reading.score}{Students reading score (integer)}
+#'   \item{writing.score}{Students writing score (integer)}
+#' }
+#'
+#' @source Mini project exam data
+"exam_data"
