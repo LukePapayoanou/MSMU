f48d394d3956219748f28d30752dc30ae76ec59b
LukePapayoanou <l.papayoanou@email.msmary.edu>
2025-06-30 21:06:11 -0700

    adding ledger data and documentation

---
+++ R/ledger_data.R
@@ -0,0 +1,11 @@
+#' Ledger data
+#'
+#'
+#' @format A data frame with 4 rows and 104 columns:
+#' \describe{
+#'   \item{color}{colors (character)}
+#'   \item{type}{age (integer)}
+#' }
+#'
+#' @source ledger data
+"ledger_data"
