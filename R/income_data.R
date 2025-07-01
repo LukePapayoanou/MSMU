2dcc69b01631184c507ebd862dd9d0fc89519fbb
LukePapayoanou <l.papayoanou@email.msmary.edu>
2025-06-30 20:51:31 -0700

    adding income scores data and documentation

---
+++ R/income_data.R
@@ -0,0 +1,14 @@
+#' Income data
+#'
+#'
+#' @format A data frame with 20 rows and 5 columns:
+#' \describe{
+#'   \item{ID}{ID (integer)}
+#'   \item{Ages}{age (integer)}
+#'   \item{years_till_Riterement.65}{Years until retirement at 65 (integer)}
+#'   \item{Salary}{Salary (integer)}
+#'   \item{Birth_weight}{Birth weight (integer)}
+#' }
+#'
+#' @source Income data
+"income_data"
