c8f598014dbf4e6f5eadf089ba8276db57a67cac
LukePapayoanou <l.papayoanou@email.msmary.edu>
2025-06-30 20:37:43 -0700

    adding course scores data and documentation

---
+++ R/course_scores.R
@@ -0,0 +1,19 @@
+#' Course scores data
+#'
+#'
+#' @format A data frame with 200 rows and 10 columns:
+#' \describe{
+#'   \item{student}{Student ID (integer)}
+#'   \item{type}{Grade type (character)}
+#'   \item{Freshman_English}{Freshman English Score/letter grade (character)}
+#'   \item{Freshman_Math}{Freshman Math Score/letter grade (character)}
+#'   \item{Sophomore_English}{Sophomore English Score/letter grade (character)}
+#'   \item{Sophomore_Math}{Sophomore Math Score/letter grade (character)}
+#'   \item{Junior_English}{Junior English Score/letter grade (character)}
+#'   \item{Junior_Math}{Junior Math Score/letter grade (character)}
+#'   \item{Senior_English}{Senior English Score/letter grade (character)}
+#'   \item{Senior_Math}{Senior Math Score/letter grade (character)}
+#' }
+#'
+#' @source course scores
+"course_scores"
