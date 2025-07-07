c726d012d47c8031bd1612a966fdd2fb68b57f8e
LukePapayoanou <l.papayoanou@email.msmary.edu>
2025-07-07 12:15:41 -0700

    updating description and source of soccer data

--- R/soccer.R
+++ R/soccer.R
@@ -1,9 +1,12 @@
-#' Historic soccer data
+#' @title Historic soccer data
+#'
+#' @description
+#' This dataset contains historical match results from various international
+#' soccer games between different countries for the years 1872-2024.
 #'
 #'
-#' @format A data frame with 13750 rows and 6 columns:
+#' @format A data frame with 13750 rows and 5 columns:
 #' \describe{
-#'   \item{X}{numbered list (integer)}
 #'   \item{date}{Date of match (character)}
 #'   \item{home_team}{Home team name (character)}
 #'   \item{away_team}{Away team name (character)}
@@ -11,5 +14,6 @@
 #'   \item{away_score}{Away teams goal count (integer)}
 #' }
 #'
-#' @source historic soccer data
+#' @source \href{https://www.kaggle.com/datasets/martj42/international-football-results-from-1872-to-2017?select=results.csv}{Kaggle International football results dataset}
+#' library with alterations made for educational purposes.”
 "soccer"
